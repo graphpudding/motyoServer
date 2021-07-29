7a1785e114bc4439d64b621e6efab9ef10d610c5
MOTYA <pblheklka@gmail.com>
2021-07-29 10:44:34 +0300

    colors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws');
 let users = [];
 let bloks = {};
+let colors = ["red","blue","green","white","yellow","black","purple","orange"]
 //wsServer.on('connection', onConnect);
 let Connection = class {
   constructor() {
@@ -10,10 +11,10 @@ let Connection = class {
     }
     onConnect(wsClient) {
       console.log('Новый пользователь');
-      users.push(wsClient)
-      console.log(this.users)
+      users.push(wsClient);
+      console.log(this.users);
       // отправка приветственного сообщения клиенту
-      wsClient.send('Привет');
+      wsClient.send(JSON.stringify({type:"message",ms:'Привет'}));
       wsClient.on('message', function(message) {
         console.log(message);
         try {
@@ -26,19 +27,17 @@ let Connection = class {
               users.forEach((item, i) => {
                 item.send(jsonMessage.data);
               });
-              break;
+            break;
             case 'newUser':
-              let ms = {name:"user",color:"1_0_1"}
-              users.forEach((item, i) => {
-                item.send(JSON.stringify(ms));
-              });
-
-              break;
+              let ms = {type:"initUser",name:"user",color: colors[0],obj: bloks};
+              colors.splice(0,1);
+              wsClient.send(JSON.stringify(ms));
+            break;
             case 'build':
-                  if(bloks[jsonMessage.data] == undefined){
-                    bloks[jsonMessage.data] = true;
+                  if(bloks[jsonMessage.name] == undefined){
+                    bloks[jsonMessage.name] = {user:jsonMessage.user,color: jsonMessage.color};
                       users.forEach((item, i) => {
-                        item.send(jsonMessage.data);
+                        item.send(JSON.stringify({type:"build",id: jsonMessage.name,color: jsonMessage.color}));
                       })
                   }
               break;
